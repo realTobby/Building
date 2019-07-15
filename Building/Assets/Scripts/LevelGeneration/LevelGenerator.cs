94c3d006a70cd5432a2ba1528e267233207e5700
Tobby <tkattanek00@gmail.com>
2019-07-15 21:40:35 +0200

    Setup Level
    - created hexagon shape in blender (thanks dad!)
    - created hexagon prefab (level field)
    - created script for basic shape creation (figuring out dimensions in the coordinate system)
    - feeling good about the project

---
+++ Building/Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public int randomTileNumber = 0;
+    public GameObject prefabHexagonBaseTile;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var parent = GameObject.FindGameObjectWithTag("baseFieldList");
+
+        randomTileNumber = Random.Range(10, 25);
+
+
+        Instantiate(prefabHexagonBaseTile, new Vector3(0,0,0), prefabHexagonBaseTile.transform.rotation, parent.transform);
+        Instantiate(prefabHexagonBaseTile, new Vector3(-1.75f, 0, 0), prefabHexagonBaseTile.transform.rotation, parent.transform);
+        Instantiate(prefabHexagonBaseTile, new Vector3(1.75f, 0, 0), prefabHexagonBaseTile.transform.rotation, parent.transform);
+
+
+        Instantiate(prefabHexagonBaseTile, new Vector3(.9f, 0, -1.55f), prefabHexagonBaseTile.transform.rotation, parent.transform);
+        Instantiate(prefabHexagonBaseTile, new Vector3(.9f, 0, 1.55f), prefabHexagonBaseTile.transform.rotation, parent.transform);
+
+        Instantiate(prefabHexagonBaseTile, new Vector3(-.9f, 0, -1.55f), prefabHexagonBaseTile.transform.rotation, parent.transform);
+        Instantiate(prefabHexagonBaseTile, new Vector3(-.9f, 0, 1.55f), prefabHexagonBaseTile.transform.rotation, parent.transform);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
